4238bdc2dfcd06c176244fcef7c38ed85eba0b4c
Troy Chryssos <troychryssos@gmail.com>
2022-02-12 22:33:47 -0500

    updating build command to hopefully dodge jekyll on github pages

---
+++ index.8b3e3062.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("art-form");globalThis.onload=async()=>{t.addEventListener("submit",(t=>{t.preventDefault();const e=new FormData(t.target);for(const t of e.values())console.log(t)}))};
+//# sourceMappingURL=index.8b3e3062.js.map
